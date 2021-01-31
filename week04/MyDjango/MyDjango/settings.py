071ea46fcc2dcce97905d23367044972db16de88
gyyy2020 <workyg@163.com>
2021-01-31 20:15:21 +0800

    week04 1.0.0

--- week04/MyDjango/MyDjango/settings.py
+++ week04/MyDjango/MyDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+pt*c=6w%p^h*^@4qrq00-n0ozm8u70kq005y__gjbzca8bl(&'
+SECRET_KEY = '6666666666666'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
